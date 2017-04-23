8f39b1eb092ceb8ae1c4cb1c13b9798ff65bd4a8
George <georgec.perez@gmail.com>
2017-04-23 09:11:08 -0400

    My shapeArea function is green

--- codefights/shapeArea/main.py
+++ codefights/shapeArea/main.py
@@ -1,14 +1,30 @@
+# mine works
 def shapeArea(n):
-    if n == 1:
+    if n == 0:
+        area = 0;
+    elif n == 1:
         area = 1
     else:
         area = (n * n) + ((n-1) * (n-1))
     return area
 
 
+print shapeArea(1)
+print shapeArea(2)
+print shapeArea(3)
+print shapeArea(5)
+
+
+# working example
 def shapeArea(n):
     if n == 1:
         area = 1
     else:
         area = shapeArea(n - 1) + (n - 1) * 4
     return area
+
+
+print shapeArea(1)
+print shapeArea(2)
+print shapeArea(3)
+print shapeArea(5)
